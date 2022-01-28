fed6007eb78d632c528688f5ba812b0f4ad3453a
bvrbryn445 <law911012eqw@gmail.com>
2022-01-27 20:05:33 -0500

    feat: add about page

--- src/modules/about.js
+++ src/modules/about.js
@@ -2,17 +2,52 @@ import { createEl, appendChildren } from '../helpers';
 import data from '../database/data.yaml';
 
 const AboutModule = (() => {
+    //texts to be used for about content
+    const about = data.about.about;
+    const journey = data.about.journey;
+
+    //create elements
     const _createAboutUs = () => {
-        const aboutWrapperEl = createEl('div', 'about-wrapper');
-        const aboutContentContainer = createEl('div', 'about-contents');
+        const aboutWrapperEl = createEl('div', ['about-wrapper', 'o-wrapper']);
+        const contentWrapper = createEl('div', ['about-content-wrapper', 'center']);
+        const contentContainer = createEl('div', 'about-content-container');
+
+        const contentInfo = createEl('section', 'about-content-info');
+        const contentHeader = createEl('h1', 'about-content-header', about.header);
+        const contentTxt = createEl('p', 'about-content-txt', about.para);
+
+        const aboutImages = createEl('section', 'about-content-images');
+        const aboutOwnerImgWrapper = createEl('div', 'about-owner-wrapper');
+        const aboutManImg = createEl('div', 'about-owner');
+        const aboutCafeImgWrapper = createEl('div', 'about-cafe-wrapper');
+        const aboutCafeImg = createEl('div', 'about-cafe');
+
+        aboutWrapperEl.append(contentWrapper);
+        contentWrapper.append(contentContainer);
+        appendChildren(contentContainer, [contentInfo, aboutImages]);
+        appendChildren(contentInfo, [contentHeader, contentTxt]);
+        aboutOwnerImgWrapper.append(aboutManImg);
+        aboutCafeImgWrapper.append(aboutCafeImg);
+        appendChildren(aboutImages, [aboutOwnerImgWrapper, aboutCafeImgWrapper]);
+
         return aboutWrapperEl;
     }
     const _createOurJourney = () => {
-        const ourJourneyEl = createEl('div', 'journey-wrapper');
+        const ourJourneyEl = createEl('div', ['journey-wrapper', 'center']);
+        const oWrapper = createEl('div', 'o-wrapper');
+        const ourJourneyContainer = createEl('div', 'journey-content-container');
+        const journeyImgWrapper = createEl('div', 'journey-img-wrapper');
+        const journeyImg = createEl('div', 'journey-content-img');
+        const journeyHeader = createEl('h1', 'journey-content-header', journey.header);
+        const journeyTxt = createEl('p', 'journey-content-txt', journey.para);
+        ourJourneyEl.append(oWrapper);
+        oWrapper.append(ourJourneyContainer);
+        journeyImgWrapper.append(journeyImg);
+        appendChildren(ourJourneyContainer, [journeyImgWrapper, journeyHeader, journeyTxt]);
         return ourJourneyEl;
     }
     const initialize = () => {
-        const about = createEl('div', ['module__about-page', 'o-wrapper']);
+        const about = createEl('div', 'module__about-page');
         appendChildren(about, [_createAboutUs(), _createOurJourney()]);
         return about;
     }
